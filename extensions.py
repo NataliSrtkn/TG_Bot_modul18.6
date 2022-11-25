7327cf79dfd8c38489ceb4d833e8bb20bce40479
NataliSrtkn <116359781+NataliSrtkn@users.noreply.github.com>
2022-11-25 20:26:52 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -30,5 +30,5 @@ class Exchange:
 
         r = requests.get(
             f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
-        total_base = float(json.loads(r.content)[keys[quote]])
+        total_base = float(json.loads(r.content)[keys[quote]]) * amount
         return total_base
